b4561f1734103f0fbfa9ab97dca165ba03d405ae
AizatKarimzhanova <aizatkarimova00@gmail.com>
2026-04-08 22:34:50 +0600

    hw2

--- product/models.py
+++ product/models.py
@@ -10,14 +10,16 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     price = models.IntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='products')
 
     def __str__(self):
         return self.title
     
 class Review(models.Model):
     text = models.TextField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, related_name='reviews')
+    stars = models.IntegerField(choices=((i,i) for i in range (1,6)), default=5, null=True)
+    #product_reviews = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, related_name='reviews')
 
     def __str__(self):
         return self.text
--- product/serializers.py
+++ product/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 from .models import Category, Product, Review
 
 class CategoryListSerializer(serializers.ModelSerializer):
+    products_count = serializers.SerializerMethodField()
     class Meta:
         model = Category
-        fields = 'id name'.split()
+        fields = 'id name products_count'.split()
+    
+    def get_products_count(self, obj):
+        return obj.products.count()
 
 class CategoryDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,9 +18,17 @@ class CategoryDetailSerializer(serializers.ModelSerializer):
 
 
 class ProductListSerializer(serializers.ModelSerializer):
+    average_rating = serializers.SerializerMethodField()
     class Meta:
         model = Product
-        fields = 'id title'.split()
+        fields = 'id title reviews average_rating'.split()
+        depth = 1     
+
+    def get_average_rating(self, obj):
+        ratings = [i.stars for i in obj.reviews.all() if i.stars is not None]
+        if ratings:
+            return sum(ratings) / len(ratings)
+        return 0 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,12 +36,10 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
-
 class ReviewListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = 'id text'.split()
+        fields = 'id text stars'.split()
 
 class ReviewDetailSerializer(serializers.ModelSerializer):
     class Meta:
